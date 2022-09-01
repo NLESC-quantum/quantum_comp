aa507dbc4a406e944b119f606ac62ae3b09ff6ec
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2022-09-01 13:20:02 +0200

    Fix some typos

--- redcal/vqls/get_b.py
+++ redcal/vqls/get_b.py
@@ -5,7 +5,12 @@ import numpy as np
 
 
 def get_b_unitmatrix(desired_vector, nqbit, decimals=6):
+    """Get unitary matrix that generates |b> from |0>
 
+    We do that by using Qiskit to create a circuit,
+    initialize the state of the circuit to |b>
+    and extract the matrix of the circuit.
+    There might be better ways of doing this."""
 
     # desired_vector /= np.linalg.norm(desired_vector)
     norm = np.linalg.norm(desired_vector)
